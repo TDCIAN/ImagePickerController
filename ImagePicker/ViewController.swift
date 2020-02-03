c474a01f27b4ac11b6cc3fa12311333a0f443d1f
KJM <tdcian@hexlant.com>
2020-02-03 09:28:48 +0900

    imagePicker

--- ImagePicker/ViewController.swift
+++ ImagePicker/ViewController.swift
@@ -8,11 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    lazy var imagePicker: UIImagePickerController = {
+        let picker: UIImagePickerController = UIImagePickerController()
+        picker.sourceType = .photoLibrary
+        picker.delegate = self // delegate -> ViewController가 picker의 델리게이트 역할을 하게 될 것이다
+        return picker
+    }()
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func touchUpSelectImageButton(_ sender: UIButton) {
+        self.present(self.imagePicker, animated: true,  completion: nil)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let originalImage: UIImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            self.imageView.image = originalImage
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+        
     }
 
 
